886a9349727a0473de4abb30c791ff0c5a7b2bb7
nwachter <malaikaninaw@yahoo.fr>
2023-11-06 23:01:03 +0100

    changement structure texte 'header' -> dans main pour création menu burger block

--- js/burger.js
+++ js/burger.js
@@ -1,19 +1,24 @@
 window.onload = function () {
 
-    var sidenav = document.getElementById("top-nav");
-    var openBtn = document.getElementById("openBtn");
-    var closeBtn = document.getElementById("closeBtn");
-    
-    openBtn.onclick = openNav;
-    closeBtn.onclick = closeNav;
-    
-    /* Set the width of the side navigation to 250px */
-    function openNav() {
-      sidenav.classList.add("active");
-    }
-    
-    /* Set the width of the side navigation to 0 */
-    function closeNav() {
-      sidenav.classList.remove("active");
-    }
+  var sidenav = document.getElementById("menu-list");
+  var openBtn = document.getElementById("openBtn");
+  var closeBtn = document.getElementById("closeBtn");
+  
+  openBtn.onclick = openNav;
+  closeBtn.onclick = closeNav;
+  
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    sidenav.classList.add("active");
+    closeBtn.classList.add("active");
+    openBtn.classList.add("inactive");
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    closeBtn.classList.add("inactive");
+    sidenav.classList.remove("active");
+    openBtn.classList.remove("inactive");
+    openBtn.classList.add("active");
+  }
 }
