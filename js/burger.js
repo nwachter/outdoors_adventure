c79bdb3f8c00cc1f1b0b5aa2bf829834f7460035
nwachter <malaikaninaw@yahoo.fr>
2023-11-25 01:30:28 +0100

    Reprise -  Menu Burger

--- js/burger.js
+++ js/burger.js
@@ -1,24 +1,55 @@
 window.onload = function () {
-
-  var sidenav = document.getElementById("menu-list");
+  var nav = document.getElementById('topnav');
+  var smallNav = document.getElementById("menu-list");
   var openBtn = document.getElementById("openBtn");
   var closeBtn = document.getElementById("closeBtn");
-  
+  var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
   openBtn.onclick = openNav;
   closeBtn.onclick = closeNav;
-  
+
   /* Set the width of the side navigation to 250px */
   function openNav() {
-    sidenav.classList.add("active");
+    smallNav.classList.add("active");
     closeBtn.classList.add("active");
-    openBtn.classList.add("inactive");
+    // openBtn.classList.add("inactive");
+    // openBtn.classList.remove("active");   A DE COMMENTER DES LES TESTS FINIS
+    nav.replaceChild(closeBtn, nav.childNodes[5]);
+    
   }
-  
+
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    closeBtn.classList.add("inactive");
-    sidenav.classList.remove("active");
-    openBtn.classList.remove("inactive");
+    // closeBtn.classList.add("inactive");
+    // openBtn.classList.remove("inactive");
+    closeBtn.classList.remove("active");
+    smallNav.classList.remove("active");
     openBtn.classList.add("active");
+
+    nav.replaceChild(openBtn, nav.childNodes[5]);
   }
+
+  //Si menu actif et changement taille browser : remove active
+
+  window.addEventListener("resize", (event) => {
+    console.log(vw);
+    if (vw > 801) {
+      console.log('declenche');
+
+      smallNav.classList.remove("active");
+      closeBtn.classList.remove("active");
+      // openBtn.classList.remove("active");
+    }
+  });
+
+  /* openBtn doit seulement ne pas etre actif quand menu ouvert; en tps normal, il est actif + placé à -100px */
+
+// if (document.readyState =! "loading") {
+//   if (vw > 801) {
+//     openBtn
+//   }
+//} 
+
+
 }
